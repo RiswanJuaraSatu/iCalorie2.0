08d368aa3bea7dc66b2cb66e4d1835fe189f771c
galihjufa <galihjufa@gmail.com>
2018-12-17 11:46:06 +0700

    design login update

--- app/src/main/java/com/apps/riswanda/icalorie/LoginActivity.java
+++ app/src/main/java/com/apps/riswanda/icalorie/LoginActivity.java
@@ -3,9 +3,14 @@ package com.apps.riswanda.icalorie;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Patterns;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,8 +28,7 @@ import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText inputEmail;
-    private EditText inputKatasandi;
+    private EditText inputEmail,inputKatasandi;
     private Button btn_logIn;
 
     private FirebaseAuth mAuth;
@@ -46,27 +50,76 @@ public class LoginActivity extends AppCompatActivity {
 
 
         inputEmail = (EditText) findViewById(R.id.inputEmail);
+        inputEmail.addTextChangedListener(emailW);
         inputKatasandi = (EditText) findViewById(R.id.inputKatasandi);
+        inputKatasandi.addTextChangedListener(pw);
 
         btn_logIn = (Button) findViewById(R.id.btn_logIn);
 
         btn_logIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(!inputEmail.getText().toString().isEmpty() && !inputKatasandi.getText().toString().isEmpty()) {
+                    checkLogin();
+                }
+                if(inputEmail.getText().toString().isEmpty()){
+                    inputEmail.setError("Email tidak boleh kosong!");
+                }
+                if(inputKatasandi.getText().toString().isEmpty()){
+                    inputKatasandi.setError("Password tidak boleh kosong!");
+                }
 
-                checkLogin();
             }
         });
 
 
     }
+    private TextWatcher emailW = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if(inputEmail.getText().toString().isEmpty()) {
+                inputEmail.setError("Email tidak boleh kosong!");
+            }
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+    private TextWatcher pw =new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if(inputKatasandi.getText().toString().isEmpty()){
+                inputKatasandi.setError("Password tidak boleh kosong!");
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+
 
     private void checkLogin() {
 
         String email = inputEmail.getText().toString().trim();
         String katasandi = inputKatasandi.getText().toString().trim();
 
-        if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(katasandi)){
+
+        if (!TextUtils.isEmpty(email)&& Patterns.EMAIL_ADDRESS.matcher(email).matches() && !TextUtils.isEmpty(katasandi)){
 
             mProgess.setMessage("Signing In");
             mProgess.show();
@@ -80,20 +133,18 @@ public class LoginActivity extends AppCompatActivity {
                         checkUserExist();
 
                     }else {
-                        Toast.makeText(LoginActivity.this, "Login Gagal", Toast.LENGTH_LONG).show();
+                        Toast.makeText(LoginActivity.this, "Login Gagal. Email atau Password salah!", Toast.LENGTH_LONG).show();
                     }
 
                     mProgess.dismiss();
 
-                    Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
-                    mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    startActivity(mainIntent);
+
 
                 }
             });
-
-
-
+        }
+        else {
+            Toast.makeText(LoginActivity.this,"Email atau Password Salah!", Toast.LENGTH_LONG).show();
         }
 
     }
