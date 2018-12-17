24addc8939c34cfa5d001ff5ced6bcc8b1319972
RiswanJuaraSatu <riswanway@gmail.com>
2018-12-17 09:45:32 +0700

    menu makan 70%

---
+++ app/src/main/java/com/apps/riswanda/icalorie/ExpandablesListAdapter.java
@@ -0,0 +1,72 @@
+package com.apps.riswanda.icalorie;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class ExpandablesListAdapter extends BaseExpandableListAdapter {
+
+    private Context context;
+    private List<String> listDataHeader;
+    private HashMap<String,List<String>> listHashMap;
+
+    public ExpandablesListAdapter(Context context, List<String> listDataHeader, HashMap<String, List<String>> listHashMap) {
+        this.context = context;
+        this.listDataHeader = listDataHeader;
+        this.listHashMap = listHashMap;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return listDataHeader.size();
+    }
+
+    @Override
+    public int getChildrenCount(int i) {
+        return listHashMap.get(listDataHeader).get(i)).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return null;
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return null;
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return 0;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return false;
+    }
+}
